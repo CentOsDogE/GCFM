c953b511b57c073a4de2129514d9c14e800758e9
CentOsDogE <anhratthichquay123@yahoo.com.vn>
2017-01-07 14:11:30 +0000

    nooooo

--- src/CentOs/GiftCode.php
+++ src/CentOs/GiftCode.php
@@ -13,13 +13,13 @@ use pocketmine\utils\Config;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat as C;
 
-class VipsPlusMain extends PluginBase implements Listener{
+class GiftCode extends PluginBase implements Listener{
 	public function onEnable(){
 		@mkdir($this->getDataFolder());
-		$this->vips = new Config($this->getDataFolder() . "codeoftoday.yml", Config::YAML);
 		$this->vips = new Config($this->getDataFolder() . "playersnormal.yml", Config::YAML);
 		$this->vips = new Config($this->getDataFolder() . "playersvip.yml", Config::YAML);
 		$this->getLogger()->info("§a" . $this->getDescription()->getFullName() . " enabled!");
+		
     }
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
 		if(count($args) === 0){
@@ -30,8 +30,11 @@ class VipsPlusMain extends PluginBase implements Listener{
 			case "member":
 			case "mems":
 			case "normal":
-				if
-				
+				if(!$sender instanceof Player){
+					$sender->sendMessage("This Command Only Works for players! Please perform this command IN GAME!");
+				} else {
+					$sender->getInventory()->addItem(Item::get(364,0,4));
+				}
 			return true;
 		}
 	}
