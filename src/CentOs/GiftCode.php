ee00b9f517ea8739bf0bb1d6e76aae27317a18c1
CentOsDogE <anhratthichquay123@yahoo.com.vn>
2017-01-08 09:47:32 +0000

    Update GiftCode.php

--- src/CentOs/GiftCode.php
+++ src/CentOs/GiftCode.php
@@ -60,7 +60,7 @@ class GiftCode extends PluginBase implements Listener{
 					return true;
 				} else {
 					if($sender->hasPermission("giftcode.members")){
-						if(($this->code->get("0")[$args[0]])){
+						if(($this->code->get()["0"])){
 							$sender->sendMessage("Yes");
 						} else {
 							$sender->sendMessage("No");
